dc75d12d506c8884d2376bce609eea9b2e96bbc4
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-01 01:30:54 -0500

    Update NFTMarketplace.sol

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -33,7 +33,7 @@ contract NFTMarketplace is ERC721URIStorage {
       bool sold
     );
 
-    constructor() ERC721("Polyplace Inc.", "PLY") {
+    constructor() ERC721("ALT NFT Market", "ANFT") {
       owner = payable(msg.sender);
     }
 
