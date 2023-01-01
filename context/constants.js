bc556c4360cce39832efc7e7a69b0987fb72876f
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-01 01:38:11 -0500

    Update constants.js

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import marketAbi from './NFTMarketplace.json';
 
-export const MarketAddress = '0x515EcD776cBb1fF41e29D1134158ecC45BC0D7C8';
+export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 export const MarketAddressABI = marketAbi.abi;
